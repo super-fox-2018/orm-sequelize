f29445f3c06b066ec8fe0c7d3fefb5b3f9729f01
Rhesa Utomo <venderblitz@gmail.com>
2018-05-18 10:46:53 +0700

    release 1 author methods

--- index.js
+++ index.js
@@ -10,12 +10,11 @@ let Tag = Model.Tag;
 let Article = Model.Article;
 
 if (table === 'author') {
-  if (command === 'add') {
-    addAuthor(args);
-  }
-  if (command === 'readOne') {
-    readOneAuthor(args);
-  }
+  if (command === 'add') { addAuthor(args) }
+  if (command === 'read_one') { readOneAuthor(args) }
+  if (command === 'read_all') { readAllAuthor() }
+  if (command === 'update') { updateAuthor(args) }
+  if (command === 'delete') { deleteAuthor(args) }
 }
 
 // if (table === 'tag') {
@@ -41,22 +40,60 @@ function addAuthor(args) {
     age: age
   })
   .then(author => {
-    console.log(author.get({ plain: true }))
+    console.log(author.get({ plain: true }));
+    process.exit();
   })
 }
 
 function readOneAuthor(args) {
   let authorId = Number(args);
-  Author.findAll({
-    where: {
-      id: authorId
-    }
+  Author.findById(authorId)
+  .then(result => {
+    console.log(result.dataValues);
+    process.exit();
   });
-  // process.exit();
 }
 
-function readAllAuthor() {}
-function updateAuthor() {}
-function deleteAuthor() {}
+function readAllAuthor() {
+  Author.findAll().then(result => { 
+    console.log(result) ;
+    process.exit();
+  })
+}
 
-// process.exit
+function updateAuthor(args) {
+  // node index author update <column-name> <value> <id>
+  let column = args[0];
+  let value = args[1];
+  let id = Number(args[2]);
+
+  Author.update({
+    [column]: value
+  }, {
+    where: {id}
+  })
+  .then((status) => { // if succeeded, will return an array with value 1
+    if (status[0] === 1) {
+      console.log(`Data dengan ID: ${id} berhasil di update`);
+    }
+  })
+  .catch((err) => { console.log(err) })
+  .then(() => process.exit());
+}
+
+function deleteAuthor(args) {
+  let authorId = args[0]
+
+  Author.destroy({
+    where: { id: authorId }
+  })
+  .then((status) => {
+    if (status[0] === 1) {
+      console.log(`Data dengan ID: ${authorId} berhasil dihapus`)
+    }
+  })
+  .catch((err) => {
+    console.log(err)
+  })
+  .then(() => process.exit())
+}
